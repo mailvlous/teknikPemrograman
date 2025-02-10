ee5186647d12435a1528ea262b5c7013d5fe8202
mailvlous <mailvelous.career@gmail.com>
2025-02-10 14:01:26 +0700

    feat: add week 1 of Teknik Pemrograman mata kuliah

---
+++ Week 1/Soal1.java
@@ -0,0 +1,19 @@
+public class Soal1 
+{
+    public static void main(String[] args) {
+        byte angka1 = 125;
+        byte angka2 = 6;
+        byte hasil = (byte) (angka1+angka2);
+
+        System.err.println("Hasil 1:"+hasil);
+    }
+}
+
+/*
+ * Permasalahan yang dihadapi: Saya kurang informasi terkait tipe data byte
+ * 
+ * Solusi dari permasalahan: Bisa lihat lewat buku dan search dengan kata kunci "byte"
+ * ataupun googling
+ * 
+ * Nama teman yang membantu:
+ */
---
+++ Week 1/Soal2.java
@@ -0,0 +1,15 @@
+public class Soal2 {
+    public static void main(String[] args) {
+        int i = 42;
+        String s = (i<40)?"life":(i>50)?"universe":"everything";
+        System.err.println(s);
+    }
+}
+
+/*
+ * Permasalahan yang dihadapi: Saya tidak tahu banyak tentang ternary operator
+ * 
+ * Solusi dari permasalahan: Namun jika dilihat lihat ini seperti if-conditional
+ * 
+ * Nama teman yang membantu:
+ */
